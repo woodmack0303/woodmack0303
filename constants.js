fdc45ab1691ff04596657362ab942d3e8aff6afc
woodmack0303 <70702098+woodmack0303@users.noreply.github.com>
2021-05-11 11:58:45 -0400

    add project files to git
    completed coding test uploaded to github

---
+++ constants.js
@@ -0,0 +1,23 @@
+var BUTTONS = {
+    COLUMN1: "1",
+    COLUMN2: "2",
+    COLUMN3: "3",
+    COLUMN4: "4",
+    COLUMN5: "5",
+    ROWA: "A",
+    ROWB: "B",
+    ROWC: "C",
+    CANCEL: "CANCEL",
+}
+
+var COINS = {
+    NICKEL: 0.05,
+    DIME: 0.10,
+    QUARTER: 0.25,
+}
+
+var ROWPRICE = {
+    A: 0.25,
+    B: 0.50,
+    C: 1.00,
+}
